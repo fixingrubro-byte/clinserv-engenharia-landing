f2612d5c82bbec567931c4c9f18bcaeafcab9590
fixingrubro-byte <fixingrubro@gmail.com>
2026-01-23 12:15:41 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "./", // 🔥 ESSA LINHA RESOLVE A TELA BRANCA
-
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
+  base: "./",
 
   plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+    react(), // 🔥 REMOVEMOS COMPLETAMENTE lovable-tagger
+  ],
 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
